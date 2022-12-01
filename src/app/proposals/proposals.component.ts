30195d051c6245ad7be72545c38b15de043b003a
egoisaway <egoisaway@gmail.com>
2022-12-01 10:44:07 -0300

    proposal generator working

--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -9,6 +9,7 @@ import { ApiService } from '../api.service';
 export class ProposalsComponent {
   title="Propostas"
   plans: Array<any> = []
+  lines: Array<any> = []
 
   constructor(private apiService:ApiService, private changeDetection: ChangeDetectorRef){}
 
@@ -20,7 +21,36 @@ export class ProposalsComponent {
     console.log()
     this.apiService.getAll("plans").subscribe((data:any)=>{
       this.plans=data.rows
-      console.log(this.plans)
     })
+  }
+
+  addLine(){
+    this.lines.push('line')
+  }
+  removeLine(){
+    this.lines.pop()
+  }
+  print(){
+    window.print()
+  }
+  updatePrice(which:string){
+    let elements = Array.from(document.getElementsByClassName(`${which}-price`))
+    let values:Array<any> = []
+    elements.forEach((element:any) => {
+      values.push(parseInt(element.value))
+    });
+    
+    let sum = values.reduce((partialSum, a) => partialSum + a, 0);
+    
+    let target:any = document.getElementById(`${which}-total`)
+    target.innerHTML = 'R$ '+sum
+  }
+  getPlanPrice(plan:any,target:any,which:any){
+    let price 
+    this.plans.find((obj:any)=>{
+      if (obj.offer == plan) price = obj.value
+    })
+    target.value = price
+    this.updatePrice(which)
   }
 }
