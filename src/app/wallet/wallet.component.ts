92de2089826e87ecc736137a21ce21bafb54608e
egoisaway <egoisaway@gmail.com>
2022-12-12 17:14:58 -0300

    access can be inputed now. also added the renovations tab.

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -7,17 +7,19 @@ import { ApiService } from '../api.service';
   styleUrls: ['./wallet.component.scss']
 })
 export class WalletComponent {
-  title = "Consultar Carteira"
   data:any = []
   results:any = []
   states:any = []
   currentAccess:any = []
+  currentAccessId:any
+  platforms:any
 
   constructor(private apiService:ApiService){}
 
   ngOnInit(){
    this.getStates()
    this.getClientsFull()
+   this.getPlatforms()
   }
 
   getStates(){
@@ -29,8 +31,11 @@ export class WalletComponent {
       data=>{
         this.data=data.rows
         this.results=this.data
-      })
-    }
+    })
+  }
+  getPlatforms(){
+    this.apiService.getAll("platforms").subscribe(data=>this.platforms=data.rows)
+  }
 
   find(route:any,params:any){
     if(params.cnpj=="") delete params.cnpj
@@ -38,6 +43,7 @@ export class WalletComponent {
     if(params.city=="") delete params.city
     if(params.state=="") delete params.state
     if(params.can_renovate==false) delete params.can_renovate
+    if(params.remaining_to_renovate=="") delete params.remaining_to_renovate
     
     this.apiService.find(route,params).subscribe(
       (res:any)=>this.results=res.rows,
@@ -46,7 +52,7 @@ export class WalletComponent {
       )
   }
 
-  show(target:any){
+  oldShow(target:any){
 
     if(target.classList.contains('showing')){
       target.classList.remove('showing')
@@ -56,12 +62,28 @@ export class WalletComponent {
       target.classList.add('showing')
     }
   }
+  show(target:any){
+    target.classList.remove('hidden')
+    target.classList.add('showing')
+  }
+  hide(target:any){
+    target.classList.remove('showing')
+    target.classList.add('hidden')
+  }
   findAccess(id:any){
-    console.log(id)
+    this.currentAccessId = id
     this.apiService.find('platforms-access/find',id).subscribe(
-      (res:any)=>this.currentAccess=res.rows,
-      err=>console.log('HTTP Error', err),
-      ()=>console.log('HTTP request completed.')
-      )
+      (res:any)=>this.currentAccess=res.rows
+    )
+  }
+  addAccess(data:any){
+    this.platforms.find((obj:any)=>{
+      if (obj.name == data.platform) data.platform_id=obj.id
+    })
+    delete data.platform
+    data.created_at = (new Date()).toISOString().split('T')[0]
+    this.apiService.post('platforms-access',data).subscribe(
+      (res:any)=>alert(res.res),
+    )
   }
 }
